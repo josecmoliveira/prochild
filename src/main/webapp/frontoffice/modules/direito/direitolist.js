efa2161afcde97226ee869ed816245b4f0cb220f
Cetire <ana.raquelfaria2001@gmail.com>
2021-05-14 10:10:26 +0100

    website temas +

--- src/main/webapp/frontoffice/modules/direito/direitolist.js
+++ src/main/webapp/frontoffice/modules/direito/direitolist.js
@@ -19,8 +19,9 @@ function getDireitosList() {
                 console.log(count);
                 for (var i=0; i<count; i++) {
                     var button = document.createElement("button");
-                    var button1 = (button.innerHTML = '<a id="link1" target="_self" href="Tópicos2.html">' + json.direito[i].nome + '</a>');
-                       var row = $('<tr><td>' + json.direito[i].direitoId+ '</td><td>' + button1 + '</td><td>' + json.direito[i].assistenteId + '</td><td><p style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box;> style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; ">' + json.direito[i].descricao + '</p></td></tr>');
+                    var button1 = (button.innerHTML = '<a id="link1" target="_self" href="tema2.html">' + json.direito[i].nome + '</a>');
+                       var row = $('<tr><td>' + json.direito[i].direitoId+ '</td><td>' + button1 + '</td><td>' + json.direito[i].assistenteId + '</td>\n\
+                       <td><p style="white-space: nowrap; max-width: 100px; overflow: hidden; text-overflow: ellipsis; ">' + json.direito[i].descricao + '</p></td></tr>');
                        $('#tabelatopicos').append(row);
                        console.log(json.lenght);
                        $('a').click ( function() {
@@ -40,3 +41,41 @@ function setId(val) {
     localStorage.setItem("id", id);
     console.log();    
 }
+
+
+function getDireito() {
+    var newid = localStorage.getItem("id");
+    $.ajax({
+            url: 'http://localhost:8080/prochild/DireitoController',
+            data: {'pwhat': 'findDireitoById', 'direitoId': newid},
+            beforeSend: function (xhr) {                
+                console.log("teste_before");
+            },
+            success: function (data) {
+                console.log("teste_success");
+                console.log(data);
+                var json = $.parseJSON(data);
+                console.log(json);
+                    document.getElementById("nomeDireito").textContent += json.direito[0].nome;
+                    document.getElementById("descricaoDireito").textContent += json.direito[0].descricao;
+                    
+                    console.log(json.direito[0].descricao);
+            }
+        });
+}
+
+
+function updateDescricao(){
+            var newid = localStorage.getItem("id");
+            $.ajax({
+                url: 'http://localhost:8080/prochild/DireitoController',
+                data: {'pwhat': 'updateDescricao', "direitoId": newid, "descricao": document.getElementById("descricaoDireito").value},
+                beforeSend: function (xhr) {                
+                    console.log("A descrição vai ser atualizada");
+                    console.log(document.getElementById("descricaoDireito").value);
+            },
+                success: function (data) {
+                    console.log("Descrição Atualizada");
+            }
+        });
+    }
